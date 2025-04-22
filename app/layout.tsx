2017f06b722af44eec28e8e40832ef8b4a6f3981
Haseeb-ur-Rehman/009 <choudharyhaseeb86@gmail.com>
2025-04-22 09:35:49 +0500

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,11 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'FitGuide',
+  description: 'The Fitness App is a modern web application designed to help users track their fitness goals, monitor progress, and stay motivated.',
+  icons: {
+    icon: '/favicon.png',
+  },
 }
 
 export default function RootLayout({
@@ -14,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head></head>
       <body>{children}</body>
     </html>
   )
